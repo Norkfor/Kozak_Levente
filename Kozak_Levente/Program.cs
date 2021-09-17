b302effb30b971d8045a6536a205bee8e1ff458d
Norkfor <norkfor@gmail.com>
2021-09-17 08:00:53 +0200

    Add project files.

---
+++ Kozak_Levente/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kozak_Levente
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
