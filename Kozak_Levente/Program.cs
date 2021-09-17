c86f3ccc42621527b72be645692b02d126154dde
Norkfor <norkfor@gmail.com>
2021-09-17 08:05:24 +0200

    Program létrehozása, feladat részek létrehozása, első feladatat megoldása.

--- Kozak_Levente/Program.cs
+++ Kozak_Levente/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Kozak_Levente
 {
@@ -10,6 +11,42 @@ namespace Kozak_Levente
   {
     static void Main(string[] args)
     {
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      HetedikFeladat();
+    }
+
+    private static void HetedikFeladat()
+    {
+
+    }
+
+    private static void HatodikFeladat()
+    {
+
+    }
+
+    private static void OtodikFeladat()
+    {
+
+    }
+
+    private static void NegyedikFeladat()
+    {
+
+    }
+
+    private static void HarmadikFeladat()
+    {
+
+    }
+
+    private static void MasodikFeladat()
+    {
+
     }
   }
 }
